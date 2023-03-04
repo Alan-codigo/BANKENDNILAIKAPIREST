da872294daa2b88057f461e3988c141dff92fd0b
Alan Ramos <alan2002050702@gmail.com>
2023-03-04 01:16:41 -0600

    Elimine carrito y solo deje pedido

--- apirest/src/main/java/com/nilaik/apirest/repository/CarritoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nilaik.apirest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nilaik.apirest.model.Carrito;
-
-public interface CarritoRepository extends JpaRepository<Carrito, Integer> {
-}
