514657f4d74c40fd014190591cc67d8d3642099d
Alan Ramos <alan2002050702@gmail.com>
2023-03-06 22:03:09 -0600

    Elimine el imgproducto

--- apirest/src/main/java/com/nilaik/apirest/repository/ImgProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nilaik.apirest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nilaik.apirest.model.ImgProducto;
-
-public interface ImgProductoRepository extends JpaRepository<ImgProducto, Long> {	
-}
